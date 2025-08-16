f93e18f48d07e3d1903479f92208ad047def9880
Qihang_awa <3319917639@qq.com>
2025-08-16 08:49:02 +0800

    完善客户端配置

--- client/global.h
+++ client/global.h
@@ -10,6 +10,8 @@
 #include <QByteArray>
 #include <QNetworkReply>
 #include <QJsonObject>
+#include <QDir>
+#include <QSettings>
 
 /**
 * @brief repolish 用来刷新qss
@@ -30,6 +32,8 @@ enum ErrorCodes{
     ERR_JSON = 1,//json解析失败
     ERR_NETWORK = 2,//网络错误
 };
+
+extern QString gate_url_prefix;
 
 
 #endif // GLOBAL_H
--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 #include <QFile>
+#include "global.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -15,6 +16,14 @@ int main(int argc, char *argv[])
     }else{
         qDebug("open failed");
     }
+
+    QString fileName = "config.ini";
+    QString app_path = QCoreApplication::applicationDirPath();
+    QString config_path = QDir::toNativeSeparators(app_path + QDir::separator() + fileName);
+    QSettings settings(config_path,QSettings::IniFormat);
+    QString gate_host = settings.value("GateServer/host").toString();
+    QString gate_port = settings.value("GateServer/port").toString();
+    gate_url_prefix = "http://" + gate_host + ":" + gate_port;
     MainWindow w;
     w.show();
     return a.exec();
